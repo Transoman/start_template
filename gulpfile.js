bdb2d34d4af8721963d17f0bbf5e908c9a2c7c2f
transoman <vasiag10@gmail.com>
2018-11-26 14:08:10 +0200

    Add new task and dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,9 @@
 var gulp         = require('gulp'),
     gp           = require('gulp-load-plugins')(),
-    // sass         = require('gulp-sass'), // Sass компілятор в css
-    // sourcemaps   = require('gulp-sourcemaps'),
-    // concat       = require('gulp-concat'), // З'єднує (конкатенує) файли
     autoprefixer = require('autoprefixer'), // PostCss autoprefixer - автоматично формує вендорні префікси
-    // cssmin       = require('gulp-cssmin'), // Мініфікація css
-    // rename       = require('gulp-rename'), // Перейменування файлу
-    // postcss      = require('gulp-postcss'), // PostCss
-    mqpacker     = require('css-mqpacker'), // Збирає всі медіа-запити в одному місці
-    // imagemin     = require('gulp-imagemin'),
-    // svgstore     = require('gulp-svgstore'),
-    // svgmin       = require('gulp-svgmin'),
-    // del          = require('del'), // Видаляє папки, файли
-    // run          = require('run-sequence'), // Запускає послідовно задачі
-    // plumber      = require('gulp-plumber'), // Відслідковування і вивід в консоль помилок
-    // notify       = require("gulp-notify"), // Вивід повідомлення про помилку
-    // cheerio      = require('gulp-cheerio'),
-    // browserify = require('browserify'),
-    // source = require('vinyl-source-stream'),
+    gcmq         = require('gulp-group-css-media-queries'),
+    runSequence  = require('run-sequence'),
+    del          = require('del'), // Збирає всі медіа-запити в одному місці
     browserSync  = require('browser-sync').create(); // Сервер
 
 // Static server
@@ -49,11 +35,9 @@ gulp.task('styles', function() {
       autoprefixer({
         browsers: ['last 5 versions'],
         cascade: false
-      }),
-      mqpacker({
-        // sort: true
       })
     ]))
+  .pipe(gcmq())
   .pipe(gulp.dest('app/css'))
   .pipe(gp.csso({
     comments: false
@@ -79,8 +63,26 @@ gulp.task('script', function() {
       'node_modules/jquery-validation/dist/jquery.validate.min.js'
     ])
   .pipe(gp.concat('script.js'))
-  // .pipe(gp.uglify())
+  .pipe(gp.uglify())
   .pipe(gulp.dest('build/js/'));
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('app/fonts/**/*')
+  .pipe(gulp.dest('build/fonts'))
+});
+
+gulp.task('images', function() {
+  return gulp.src('app/img/**/*.*')
+    // .pipe(imagemin([
+    //  imagemin.optipng({optimizationLevel: 3}),
+    //  imagemin.jpegtran({progressive: true})
+    // ]))
+    .pipe(gulp.dest('build/img'))
+});
+
+gulp.task('clean', function() {
+  del.sync('build');
 });
 
 gulp.task('svg', function() {
@@ -109,56 +111,45 @@ gulp.task('svg', function() {
   .pipe(gulp.dest('build/img'));
 });
 
-// gulp.task('symbols', function() {
-//   return gulp.src('app/img/icon/*.svg')
-//     .pipe(svgmin())
-//     .pipe(svgstore({
-//       inlineSvg: true
-//     }))
-//     .pipe(cheerio({
-//       run: function($) {
-//         $('[fill]').removeAttr('fill');
-//         $('[style]').removeAttr('style');
-//         $('[class]').removeAttr('class');
-//         $('title').remove();
-//         $('defs').remove();
-//         $('style').remove();
-//         $('svg').attr('style', 'display:none');
-//       }
-//     }))
-//     .pipe(rename('symbols.html'))
-//     .pipe(gulp.dest('app/img'));
-// });
 
 gulp.task('watch', function() {
   gulp.watch('app/sass/**/*.sass', ['styles']);
-  gulp.watch('app/*.html', ['html']);
+  gulp.watch('app/**/*.{html,php}', ['html']);
+  gulp.watch('app/fonts/**/*', ['fonts']);
+  gulp.watch('app/img/**/*.{png,jpg,svg}', ['images']);
   gulp.watch('app/js/common.js', ['js']);
 });
 
 
 /* Project transfer to production */
-gulp.task('clean', function() {
-  return del.sync('dist');
+// gulp.task('clean', function() {
+//   return del.sync('dist');
+// });
+
+
+// gulp.task('build', ['clean', 'styles', 'svg'], function(){
+//   gulp.src(['app/css/style.min.css'])
+//     .pipe(gulp.dest('dist/css'));
+
+//   gulp.src(['app/fonts/**/*'])
+//     .pipe(gulp.dest('dist/fonts'));
+
+//   gulp.src(['app/js/**/*'])
+//     .pipe(gulp.dest('dist/js'));
+
+//   gulp.src(['app/img/symbols.html'])
+//     .pipe(gulp.dest('dist/img'));
+
+//   gulp.src(['app/*.html'])
+//     .pipe(gulp.dest('dist'));
+// });
+
+
+gulp.task('default', function() {
+  runSequence(
+    'clean',
+    ['html', 'styles', 'script', 'js', 'fonts', 'images', 'svg'],
+    'watch',
+    'browser-sync'
+  )
 });
-
-
-gulp.task('build', ['clean', 'styles', 'svg'], function(){
-  gulp.src(['app/css/style.min.css'])
-    .pipe(gulp.dest('dist/css'));
-
-  gulp.src(['app/fonts/**/*'])
-    .pipe(gulp.dest('dist/fonts'));
-
-  gulp.src(['app/js/**/*'])
-    .pipe(gulp.dest('dist/js'));
-
-  gulp.src(['app/img/symbols.html'])
-    .pipe(gulp.dest('dist/img'));
-
-  gulp.src(['app/*.html'])
-    .pipe(gulp.dest('dist'));
-});
-
-
-gulp.task('default', ['html', 'styles', 'script', 'js', 'watch', 'browser-sync']);
